c1d4b79aa5ea6312d9f03a9c8f9b409872db1fdc
AnnaTurashova <30anna30.08@gmail.com>
2020-07-05 16:22:48 +0300

    Add: admin

--- main/models.py
+++ main/models.py
@@ -1,5 +1,11 @@
 # Всяк таблички БД
+# Чтобы создать таблички, нам надо здесь создать новый класс
+from django.db import models # Без неё не можем обращаться к models
 
-from django.db import models
 
-# Create your models here.
+class Task(models.Model):                                     # Имя Task, наследуем всё от models от его класса Model
+    title = models.CharField('Название', max_length=50)       # CharField для текста до 255 символов
+    task = models.TextField('Описание')                       # TextField для больших текстов
+
+    def __str__(self):    # Метод, который вызывается тогда, когда мы выводим на экран объект этого класса
+        return self.title
--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 # Сами создали
-# Отслеживание url адресов
+# Отслеживание url адресов, легко поменять url-адрес
 
 from django.urls import path
-from . import views # . - это значит из этой же директории
+from . import views    # . - это значит из этой же директории
 
 urlpatterns = [
-    path('', views.index),                 # Пустая строка - переход на главную страничку # В файле views функцию index
-    path('about-us', views.about),
+    path('', views.index, name='home'),                 # Пустая строка - переход на главную страничку # В файле views функцию index
+    path('about', views.about, name='about'),
     path('x', views.xxx)
     # можно ещё прописывать, '/' не нужно в начале
 ]
